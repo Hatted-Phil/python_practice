9c3d7ff0b9fe4afeada9694dddcbc35ec1696340
Hatted-Phil <70901782+Hatted-Phil@users.noreply.github.com>
2020-09-08 16:03:43 +0100

    Update numguess.py

--- numguess.py
+++ numguess.py
@@ -9,21 +9,21 @@ Created on Mon Sep  7 09:59:18 2020
 Low = 0
 High = 100
 answer = ''
-print('Please think of a number between 0 and 100!')
+print('Pick a number from 0 to 100!')
 while True:
     guess = (High + Low) //2
 
-    print('Is your secret number ' + str(guess))
-    print("Enter 'h' to indicate the guess is too high.", end=' ') 
-    print("Enter 'l' to indicate the guess is too low.", end=' ')
-    answer = input("Enter 'c' to indicate I guessed correctly.")
+    print('Is the number you chose ' + str(guess) + '?')
+    print("Enter 'h' if the guess is too high.", end=' ') 
+    print("Enter 'l' if the guess is too low.", end=' ')
+    answer = input("Enter 'r' if I guessed right.")
     if answer == 'h':        
      High = guess
     elif answer == 'l':
      Low = guess
-    elif answer == 'c':
-     print("Game over. Your secret number was:" + str(guess))
+    elif answer == 'r':
+     print("Game over. The number you thought of was:" + str(guess))
      break
     else:
-     print('h, l or c, dingbat?')
+     print('h, l or r, numbskull?')
     
